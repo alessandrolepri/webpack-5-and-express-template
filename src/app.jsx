f8d0b122bd76ccea62e8c471d2bc4e63a1528141
alex <alex.lepri84@gmail.com>
2021-04-28 14:37:42 +0100

    Template completed

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,20 @@
 import React from "react";
-import ReactDOM from 'react-dom';
-
-import "@babel/polyfill";
+import ReactDOM from "react-dom";
 
 import * as serviceWorker from "./serviceWorker";
 
 import "./module.scss";
-import "./style/style.scss";
-
-import ReactImg from "../assets/react.jpg";
+// import "./style/style.scss";
 
 class App extends React.Component {
-    render() {
-        return (
-            <div>Hello World
-                <img src={ReactImg} alt="REACT APP" />
-            </div>
-        )
-    }
+  render() {
+    return <h1>Hello World</h1>;
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
